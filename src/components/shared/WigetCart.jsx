c99d8ec8753daf6c7d3aa66bcc0b6857bc090a00
Alejandro Gonzalez Velasquez <alejo.gv06@gmail.com>
2023-03-04 16:39:08 -0500

    s

--- src/components/shared/WigetCart.jsx
+++ src/components/shared/WigetCart.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-
-
-
 import './styles/CartWidget.css';
 
 const CartWidget = () => {
-  //la lógica va siempre antes del return
+ 
   return (
     <div className="cart-widget"> 
       <div className="qty-display">5</div>
